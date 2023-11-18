985b87813e5bb335cc3467e65b7409a186493aa5
gosttavo <gustavogoulart92@gmail.com>
2023-11-18 20:23:11 -0300

    created project, added react router lib and tailwind css lib

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./routes/Home";
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    }
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
---
+++ src/routes/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+        <h1 className="font-bold">Hello World</h1>
+        </>
+    )
+}
