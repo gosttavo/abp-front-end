6cd232a5c1aa4cfc8e3544fc287f114034e3f267
gosttavo <gustavogoulart92@gmail.com>
2023-12-06 01:13:01 -0300

    tentei deployar e falhei

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,33 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/auth/AuthContext";
 import Home from "./routes/Home";
 import RootLayout from "./layout/RootLayout";
 import PerfilRestaurante from "./routes/PerfilRestaurante";
 import Restaurantes from "./routes/Restaurantes";
 import PaginaNaoEncontrada from "./routes/PaginaNaoEncontrada";
 import Login from "./routes/Login";
-import { AuthProvider } from "./context/auth/AuthContext";
 import Register from "./routes/Register";
 import PerfilUsuario from "./routes/PerfilUsuario";
 
 function App() {
-
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <RootLayout />,
-      children: [
-        {
-          path: '/',
-          element: <Home />
-        },
-        {
-          path: '/restaurantes',
-          element: <Restaurantes />
-        },
-        {
-          path: '/restaurantes/:id',
-          element: <PerfilRestaurante />
-        },
-        {
-          path: '/cidades',
-          element: <PerfilRestaurante />
-        },
-        {
-          path: '/perfil',
-          element: <PerfilUsuario />
-        }
-      ]
-    },
-    {
-      path: '/login',
-      element: <Login />
-    },
-    {
-      path: '/register',
-      element: <Register />
-    },
-    {
-      path: '*',
-      element: <PaginaNaoEncontrada />
-    }
-  ]);
-
   return (
-    <>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </>
-  )
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/restaurantes" element={<Restaurantes />} />
+            <Route path="/restaurantes/:id" element={<PerfilRestaurante />} />
+            <Route path="/cidades" element={<PerfilRestaurante />} />
+            <Route path="/perfil" element={<PerfilUsuario />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<PaginaNaoEncontrada />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
