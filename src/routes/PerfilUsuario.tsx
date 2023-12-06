15e1d2e98384dfac2469756278266a054720b8ee
VitorLidorio <133905864+VitorLidorio@users.noreply.github.com>
2023-12-06 12:24:40 -0300

    Update PerfilUsuario.tsx

--- src/routes/PerfilUsuario.tsx
+++ src/routes/PerfilUsuario.tsx
@@ -6,13 +6,18 @@ export default function PerfilUsuario() {
     return (
         <>
             <div className='bg-purple-500 text-white p-6 flex flex-col rounded-md'>
-                <p className='font-bold text-3xl'>
-                    {auth?.usuario?.nome} {auth?.usuario?.sobrenome}
-                </p>
+                <div className='flex justify-between'>
+                    <p className='font-bold text-3xl'>
+                        {auth?.usuario?.nome} {auth?.usuario?.sobrenome}
+                    </p>
+                    <button className='bg-white rounded-md text-purple-800 p-2 font-semibold hover:bg-slate-200 transition'>
+                        Editar
+                    </button>
+                </div>
+
                 <p className='font-semibold text-xl mt-1'>
                     {auth?.usuario?.email}
-                </p>
-                
+                </p>  
             </div>
         </>
     )
